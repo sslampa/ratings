4070495f66e8aaeb6f2afb2e91390d4005c25747
Stephen Lampa <sslampa@gmail.com>
2017-08-15 10:26:51 -0700

    Add test for multiple panels

--- test/client/List.spec.js
+++ test/client/List.spec.js
@@ -2,9 +2,9 @@ import chai from 'chai';
 import React from 'react';
 import {List} from '../../client/components/List'
 import {shallow} from  'enzyme';
-
 let expect = chai.expect;
-const items = [
+
+const item = [
   {
     id: 1,
     title: 'Louie',
@@ -13,14 +13,34 @@ const items = [
   }
 ]
 
+const items = [
+  {
+    id: 1,
+    title: 'Louie',
+    year: '2009',
+    desc: 'A comedy show'
+  },
+  {
+    id: 2,
+    title: 'Breaking Bad',
+    year: '2009',
+    desc: 'A dramatic show'
+  }
+]
+
 describe('<List />',  () => {
   it('renders', () => {
-    const component = shallow(<List items={items}/>);
+    const component = shallow(<List items={item}/>);
     expect(component.exists()).equal(true);
   });
 
   it('renders one item', () => {
-    const component = shallow(<List items={items}/>);
+    const component = shallow(<List items={item}/>);
     expect(component.find('.list-item')).to.have.length(1);
   });
+
+  it('renders mulitple items', () => {
+    const component = shallow(<List items={items}/>);
+    expect(component.find('.list-item')).to.have.length(2);
+  })
 })
