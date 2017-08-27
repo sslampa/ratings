5dd4779a78c4cc8e7fcad174b68a7cc8ccbc60a2
sslampa <sslampa@gmail.com>
2017-08-27 12:11:56 -0700

    Fix routes for users

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ import (
 
 func main() {
 	port := flags()
-	http.HandleFunc("/users/", handlers.UserRoute)
+	http.HandleFunc("/users/add", handlers.PostUserHandler)
+	// http.HandleFunc("/users/", handlers.UserRoute)
+	http.HandleFunc("/users", handlers.UsersHandler)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
--- server/handlers/user_routes.go
+++ server/handlers/user_routes.go
@@ -7,19 +7,10 @@ import (
 // UserRoute routes to correct handler
 func UserRoute(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case "POST":
-		PostUserHandler(w, r)
-		return
 	case "DELETE":
 		DeleteUserHandler(w, r)
 		return
 	}
 
-	// re := regexp.MustCompile("^.*/users/$")
-	// if re.MatchString(r.URL.Path) {
-	//   return UsersHandler
-	// }
-
-	UsersHandler(w, r)
 	return
 }
